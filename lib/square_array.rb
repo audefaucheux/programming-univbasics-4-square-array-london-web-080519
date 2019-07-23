4613b1c830c1ef5ffdb346bc23d5241efec3d32e
Aude Faucheux <aude53@gmail.com>
2019-07-23 12:34:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  count = 0
+  while array.length < count do
+    original_element = array[count]
+    new_element = original_element **
+    array[count] = new_element
+    count += 1
+  end
+  return array
 end
